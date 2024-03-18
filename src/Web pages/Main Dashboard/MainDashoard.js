a07956b84123f679e26f0cb0ede2b12ab1f92580
unknown <awonugaoladimeji@gmail.com>
2024-03-19 00:30:12 +0100

    New

--- src/Web pages/Main Dashboard/MainDashoard.js
+++ src/Web pages/Main Dashboard/MainDashoard.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import classes from '../../Web pages/Main Dashboard/MinDashboard.module.css';
 import RegLogo from '../../Images/RegistrationLogo.svg'
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 import { Tab, Tabs, Form } from 'react-bootstrap';
 import Folder from '../../Images/folder-2.svg';
 import axios from 'axios';
@@ -12,67 +12,75 @@ import LogOutIcon from '../../Images/Dashbord-menu-icons/logout.svg';
 
 
 export default function MainDashoard() {
+    const location = useLocation();
     const [activeLink, setActiveLink] = useState(null);
 
-    const handleLinkClick = (linkName) => {
-        setActiveLink(linkName);
-    };
+
+    useEffect(() => {
+        const pathname = location.pathname;
+        if (pathname === '/dashboard') {
+            setActiveLink('Dashboard');
+        } else if (pathname.includes('loan')) {
+            setActiveLink('Loan');
+        } else if (pathname.includes('grant')) {
+            setActiveLink('Grants');
+        } else if (pathname === '/') {
+            setActiveLink('Invoices');
+        } else if (pathname === '/sign_in') {
+            setActiveLink('Logout');
+        } else {
+            setActiveLink(null);
+        }
+    }, [location]);
+    
+
 
   return (
     <div className={classes.sideNavBody}>
-            {/* <div className={classes.regContainer}> */}
-                <div className={classes.sideNav}>
-                    <div className={classes.logoCont}>
-                        <img src={RegLogo} alt='Logo' />
-                    </div>
-                    <div className={classes.regMenu}>
+            <div className={classes.sideNav}>
+                <div className={classes.logoCont}>
+                    <img src={RegLogo} alt='Logo' />
+                </div>
+                <div className={classes.regMenu}>
                     <Link
                         to={'/dashboard'}
-                        onClick={() => handleLinkClick('Dashboard')}
                         className={activeLink === 'Dashboard' ? classes.active : ''}
                     >
-                        <p><img src={messageIcon} alt='icon'/>Dashboard</p>
+                        <p><img src={messageIcon} alt='icon' />Dashboard</p>
                     </Link>
                     <Link
                         to={'/loans'}
-                        onClick={() => handleLinkClick('Loan')}
                         className={activeLink === 'Loan' ? classes.active : ''}
                     >
-                        <p><img src={messageIcon} alt='icon'/> Loans</p>
+                        <p><img src={messageIcon} alt='icon' /> Loans</p>
                     </Link>
                     <Link
                         to={'/grant'}
-                        onClick={() => handleLinkClick('Grants')}
                         className={activeLink === 'Grants' ? classes.active : ''}
                     >
-                        <p> <img src={messageIcon} alt='icon'/> Grants</p>
+                        <p> <img src={messageIcon} alt='icon' /> Grants</p>
                     </Link>
                     <Link
                         to=''
-                        onClick={() => handleLinkClick('Invoices')}
                         className={activeLink === 'Invoices' ? classes.active : ''}
                     >
-                        <p> <img src={Invoice} alt='icon'/> Invoices</p>
+                        <p> <img src={Invoice} alt='icon' /> Invoices</p>
                     </Link>
                     <Link
-                        to= {'/sign_in'}
-                        onClick={() => handleLinkClick('Logout')}
+                        to={'/sign_in'}
                         className={activeLink === 'Logout' ? classes.active : ''}
                     >
-                        <p><img src={LogOutIcon} alt='icon'/> Logout</p>
+                        <p><img src={LogOutIcon} alt='icon' /> Logout</p>
                     </Link>
-                    </div>
-                </div>
-                <div className={classes.formSection}>
-                    <div className={classes.formSectionHeader}>
-                        <h1>Welcome Oriade</h1>
-                        <p>Apply for grants or loans from the Ogun state govt</p>
-                        {/* <div className={`${classes.formSecCont} ${classes.shadow}`}>
-                            <h3>Complete Registration</h3>
-                        </div> */}
-                    </div>
                 </div>
             </div>
+            <div className={classes.formSection}>
+                <div className={classes.formSectionHeader}>
+                    <h1>Welcome Oriade</h1>
+                    <p>Apply for grants or loans from the Ogun state govt</p>
+                </div>
+            </div>
+        </div>
         // </div>
     )
 }
